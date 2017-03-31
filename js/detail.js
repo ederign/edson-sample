e4f051174df90985f7df23c8e493e8cedf41dd2d
Eder Ignatowicz <ignatowicz@gmail.com>
2017-03-31 18:19:56 -0300

    Breadcrumbs, dybamic page

---
+++ js/detail.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var label_id = getUrlParameter('label_id');
+    var breadcrumb_label = getUrlParameter('breadcrumb_label');
+
+    $("#results > *").css('display','none');
+    $("#results > #"+label_id).css('display','');
+
+    $('.breadcrumbs #breadcrumb-value').text(breadcrumb_label)
+
+});
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
---
+++ js/lib.js
@@ -0,0 +1,6 @@
+function labelDetail(url, label_id,breadcrumb_label){
+    if(url==='drools75'){
+        window.location.href="drools75detail.html?label_id="+label_id+"&breadcrumb_label="+breadcrumb_label;
+    }
+}
+
